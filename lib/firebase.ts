8cadf5855bbd6a96623c85385ba071d07f78e0bd
Guild Acre Setup <admin@guildacre.com>
2026-05-02 22:12:34 +0530

    final firebase fix

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,14 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBvfKQOVBjqiSipOkM1woYLS1m5XqpFkRw",
+  apiKey: "AIzaSyBvfKOQVBjqiSipOkM1woYlS1m5XqpFkRw",
   authDomain: "guildacre.firebaseapp.com",
   projectId: "guildacre",
-  storageBucket: "guildacre.appspot.com", // ✅ FIXED
+  storageBucket: "guildacre.firebasestorage.app",
   messagingSenderId: "778744088416",
-  appId: "1:778744088416:web:635fbff47b86a37b590005",
+  appId: "1:778744088416:web:635fbff47b86a37b590005"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
