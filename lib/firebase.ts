d673bce53ff19f1dcbe3bf103ffe495116464678
Guild Acre Setup <admin@guildacre.com>
2026-05-02 22:02:17 +0530

    fix firebase config storage bucket

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,13 +1,11 @@
-// lib/firebase.ts
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvfK0QVBjqiSip0kM1woY1S1m5XqpFkRw",
+  apiKey: "AIzaSyBvfKQOVBjqiSipOkM1woYLS1m5XqpFkRw",
   authDomain: "guildacre.firebaseapp.com",
   projectId: "guildacre",
-  storageBucket: "guildacre.firebasestorage.app",
+  storageBucket: "guildacre.appspot.com", // ✅ FIXED
   messagingSenderId: "778744088416",
   appId: "1:778744088416:web:635fbff47b86a37b590005",
 };
